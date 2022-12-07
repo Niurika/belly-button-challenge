a05f216e04a806ed669f7f4cc18c68d1e6820347
Niurika Gonzalez <niurikag@gmail.com>
2022-12-06 23:35:46 -0600

    creating bubble chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -41,43 +41,75 @@
 
 // function that contains instructions at page load/refresh
 // function does not run until called
+
+
+
 function init(){
     // code that runs once (only on page load or refresh)
     
     const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
-    const dataPromise = d3.json(url);
-    console.log("Data Promise: ", dataPromise);
+   
     
-    d3.json(url).then(data => {
-        console.log(data.names)
-    })
-    let options = dataPromise.names
+    data = d3.json(url);
 
+   
+   
     // this checks that our initial function runs.
     console.log("The Init() function ran")
 
     // create dropdown/select
-    d3.select("#selDataset")
+    var optionMenu = d3.select("#selDataset")
+    data.then(data => {data.names
+        for (let i = 0; i < data.names.length; i++) { 
+            option = document.createElement('option');
+            option.text = data.names[i];
+            optionMenu.append("option")
+            .attr('value', option.text)
+            .text(option.text);
+            
+        }})
+        
+        //     data.then(data => {data.names
+        //         optionMenu.append("option")
+        //         .attr('value', data)
+        //         .text();
+               
+                
+            
+            
+        //  });
+        
+        
+        
+        
+        // };
+    
         // .on('change', () => update(newID))
-        .selectAll('options')
+        // .selectAll('option')
         // .data(dataPromise.names)
-        // .enter()
-        //.append('select')
-        //.attr('value', dataPromise )
-        .text('940');
+        // .enter()s
+        // //.append('select')
+        // .attr('value', dataPromise )
+        // .text('940');
 
+
+     
+        
+        
+   
     // run functions to generate plots
     createScatter('940')
     createBar('940')
     createSummary('940')
   
+ }
 
-}
 
 // function that runs whenever the dropdown is changed
 // this function is in the HTML and is called with an input called 'this.value'
 // that comes from the select element (dropdown)
 function optionChanged(newID){
+    console.log(newID);
     // code that updates graphics
     // one way is to recall each function
     createScatter(newID)
@@ -87,9 +119,60 @@ function optionChanged(newID){
 }
 
 function createScatter(id){
+   
+   
+   
     // code that makes scatter plot at id='bubble'
+    
+    let otu_ids = []
+    let sample_values = []
+    //let names = []
+    data.then(data => {data.samples
+    for (let i = 0; i < data.samples.length; i++) {
+        row = data.samples[i];
+        // names.push(row.pair);
+        otu_ids.push(row.otu_ids);
+        sample_values.push(row.sample_values);
+        
+    }})
+      
+    
+    
+      let trace1 = {
+            x: otu_ids,
+            y: sample_values,
+          type: 'bubble'
+        };
+        
+        let data_scatter = [trace1];
+        
+        
+        
+        Plotly.newPlot('bubble', data_scatter);
+    
+    
+    
+    
+    
+    // var optionMenu = d3.select("#bubble")
+    // data.then(data => {data.samples
+    //     for (let i = 0; i < data.samples.length; i++) { 
+    //         option = document.createElement('option');
+    //         option.text = data.samples[id];
+    //         optionMenu.append("option")
+    //         .attr('value', option.text)
+    //         .text(option.text);
+    //     }}) 
 
-    // checking to see if function is running
+   
+  
+   
+   
+   
+   
+   
+   
+        // checking to see if function is running
     console.log(`This function generates scatter plot of ${id} `)
 }
 
@@ -126,4 +209,4 @@ init()
 //                  make your chart code.
 
 // Overall, the above are the two steps you need to do (1.  Make plots with data, 2. make dropdown that passes id to functions)
-// You could do it in either order.
+// You could do it in either order
